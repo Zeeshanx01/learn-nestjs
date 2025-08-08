6711c5a163e70431110ed17c64dab4fe362279a2
Zeeshanx01 <zeeshanmunir13579@gmail.com>
2025-08-08 18:29:12 +0500

    2025-08-08_18:29

---
+++ src/users/users.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Get, Param, Post, Body, Patch, Delete, Query } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+
+  @Get() // GET /users OR /users?role=value&age=12
+  findAll(@Query() role?: 'INTERN' | 'ADMIN' | 'USER') 
+  {
+    return [];
+  }
+
+  @Get('interns') // GET /users/interns
+  findAllInterns() {
+    return [];
+  }
+
+  @Get(':id') // GET /users/:id //* always put the id route at the end.
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() // POST /users
+  create(@Body() user: {}) {
+    return user
+  }
+
+  @Patch(':id') // PATCH /users/:id 
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') // DELETE /users/:id 
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+
+
+
+
+}
